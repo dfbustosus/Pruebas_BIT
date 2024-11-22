ca7d97e54d90343fc9864b59ea1b2b05e2269bd8
David BU <dbustos@level60consulting.com>
2024-11-21 21:51:12 -0300

    xxxx

--- persona.py
+++ persona.py
@@ -2,13 +2,13 @@ import logging
 
 class Person:
     def __init__(self, name: str, phone: str, email: str, surname: str):
-        self.name = name
+            self.name = name
         self.phone = phone
         self.email = email
         self.surname = surname
 
     def __str__(self):
-        return f"Name: {self.name}, Phone: {self.phone}, Email: {self.email}"
+            return f"Name: {self.name}, Phone: {self.phone}, Email: {self.email}"
     
     def __call__(self):
         pass
