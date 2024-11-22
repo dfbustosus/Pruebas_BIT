06e044b9f4d4d56b21d41ac3af1b3e8f3bc0ebd4
David BU <dbustos@level60consulting.com>
2024-11-21 21:33:51 -0300

    feature/Class_person

---
+++ persona.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, name: str, phone: str, email: str):
+        self.name = name
+        self.phone = phone
+        self.email = email
+
+    def __str__(self):
+        return f"Name: {self.name}, Phone: {self.phone}, Email: {self.email}"
